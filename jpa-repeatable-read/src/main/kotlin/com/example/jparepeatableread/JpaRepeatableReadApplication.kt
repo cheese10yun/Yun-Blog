1bc3b107fa46697630fc03ca1f7e86c72c599762
cheese10yun <cheese10yun@gmail.com>
2021-06-19 13:54:15 +0900

    first-commit

---
+++ jpa-repeatable-read/src/main/kotlin/com/example/jparepeatableread/JpaRepeatableReadApplication.kt
@@ -0,0 +1,11 @@
+package com.example.jparepeatableread
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JpaRepeatableReadApplication
+
+fun main(args: Array<String>) {
+	runApplication<JpaRepeatableReadApplication>(*args)
+}
